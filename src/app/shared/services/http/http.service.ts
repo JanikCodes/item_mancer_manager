df79422b69126b8b315e13315e3d7312aa82b0c4
JanikCodes <piefke2000@gmx.de>
2024-07-15 16:09:26 +0200

    feat: added auto logout upon unauthorized request

--- src/app/shared/services/http/http.service.ts
+++ src/app/shared/services/http/http.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { SnackBarService } from '../snack-bar/snack-bar.service';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +18,9 @@ export class HttpService {
 
   public constructor(
     private http: HttpClient,
-    private snackBarService: SnackBarService
+    private snackBarService: SnackBarService,
+    private authService: AuthService,
+    private router: Router
   ) {}
 
   public get<T>(
@@ -27,12 +35,29 @@ export class HttpService {
     }
   ): Observable<T> {
     const appendedUrl = `${this.backendUrl}/${url}`;
-    return this.http.get<T>(appendedUrl, options);
+    return this.http
+      .get<T>(appendedUrl, options)
+      .pipe(catchError(this.handleError.bind(this)));
   }
 
   public post(url: string, body: any | null): Observable<Object> {
     const appendedUrl = `${this.backendUrl}/${url}`;
 
-    return this.http.post(appendedUrl, body);
+    return this.http
+      .post(appendedUrl, body)
+      .pipe(catchError(this.handleError.bind(this)));
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.status === 401) {
+      this.authService.clearUser();
+      this.router.navigateByUrl('');
+    } else {
+      this.snackBarService.showSnackBar('Oop. Something went wrong..', '', {
+        duration: 3000,
+      });
+    }
+
+    return throwError(() => new Error('Something went wrong'));
   }
 }
